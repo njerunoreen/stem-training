1b2e0a8914aac5023991501383c180f2211e474e
njerunoreen <105698847+njerunoreen@users.noreply.github.com>
2022-05-20 11:25:30 +0300

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,13 @@
+num1 = input("Enter first number")
+num2 = input("Enter second number")
+# re-declare variables
+
+num1 = int(num1)
+num2 = int(num2)
+
+# create sum variable
+
+sum = num1+num2
+print(sum)
+mult = num1*num2
+print(mult)
---
+++ assignment_two.py
@@ -0,0 +1,20 @@
+from tokenize import Number
+
+
+x = 10
+if  x < 5 :
+    print(x) 
+print("njerunoreen")
+ 
+ #complex calc
+op=int(input("Enter first Number"))
+op_two =int(input("Enter second Number"))
+op_three=(input("Enter operator"))
+if "*"== op_three:
+    print(op*op_two)
+elif "/" == op_three :
+    print(op/op_two)   
+
+       
+        
+       
