ac952861885fc6f9e1f43a0d6a987dc2ca1f09db
njerunoreen <105698847+njerunoreen@users.noreply.github.com>
2022-05-21 08:51:26 +0300

    Add files via upload

---
+++ assignment_three.py
@@ -0,0 +1,19 @@
+#complex calculator
+op=int(input("enter first number"))
+op_two=int(input("enter second number"))
+op_three=(input("enter operator"))
+if "/"== op_three :
+    print(op/op_two)
+ 
+elif "*" == op_three :
+    print(op*op_two)
+ 
+if  "-" == op_three : 
+
+      print(op-op_two)
+elif "+"  == op_three : 
+    print(op+op_two)  
+      
+
+    
+
