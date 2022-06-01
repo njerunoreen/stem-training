4a3f234f5bc720697de1d14dbe126c645ac0432b
njerunoreen <105698847+njerunoreen@users.noreply.github.com>
2022-06-01 14:45:30 +0300

    Add files via upload

---
+++ editing.py
@@ -0,0 +1,20 @@
+# opening and editing files  in python (.txt , .py)
+
+role_file = open ("Role.txt", "r")
+print(role_file . readline())
+#close file .
+role_file.close()
+
+file = open ("Role.txt" , "a")
+file .write ("icea lion")
+file.close()
+
+
+file = open ("role.txt" , "a")
+file .write ("icea lion")
+file.close()
+
+print("\n")
+print("n")
+print("\t")
+print("t")
