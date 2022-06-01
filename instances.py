b7ff6fe29e046e7020e82b9b6ac34b4823b31f0a
njerunoreen <105698847+njerunoreen@users.noreply.github.com>
2022-06-01 14:52:22 +0300

    Add files via upload

---
+++ instances.py
@@ -0,0 +1,13 @@
+
+from school import School
+from inventory . store import remainder 
+school_one = School (414,"nairobi")
+school_two = School (566,"mombasa")
+school_three = School (123 ,"kisumu")
+
+print(school_one.location,school_one.no_of_student)
+print(school_two.location,school_two.no_of_student)
+print(school_three.location,school_three.no_of_student)
+
+
+
---
+++ school.py
@@ -0,0 +1,10 @@
+class School :
+    def __init__(self,no_of_student,location):
+        self.no_of_student = no_of_student
+        self.location = location
+        
+    def say_motto(self):
+        print ("The sky is the limit")    
+        
+        
+    
